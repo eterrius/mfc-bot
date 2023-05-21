11a1866337c96a984a7d9185651eb233b1295a39
Tee Yee Cheng <2b03.2123251.teeyeecheng@gmail.com>
2023-05-21 13:35:38 +0800

    Added commands

---
+++ commands/utils/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		const sent = await interaction.deferReply({ fetchReply: true });
+        await interaction.followUp(`:ping_pong: ${interaction.user} Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:sparkling_heart: Websocket heartbeat: ${interaction.client.ws.ping}ms.\n:round_pushpin: Rountrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+    },
+};
