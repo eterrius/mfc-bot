6c7f56ac4cd5705a24453be4a65b401ed8908af3
Yee Cheng <jonestee18@gmail.com>
2023-05-29 10:06:42 +0800

    Add restrictions to mfc-updates

--- commands/utils/mfc-updates.js
+++ commands/utils/mfc-updates.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionsBitField } = require('discord.js');
 const { EmbedBuilder } = require('@discordjs/builders');
 const fs = require('fs')
 const config = require('../../config.json')
@@ -28,74 +28,84 @@ module.exports = {
         .setDMPermission(false),
 
     async execute(interaction) {
-        const channel = interaction.options.getChannel('channel');
-        const subcommand = interaction.options.getSubcommand();
-        
-        if (subcommand === 'add') {
-            const webhook = await channel.createWebhook({
-                name: 'MFC Updates'
-            });
-    
-            const embed = new EmbedBuilder()
-                .setTitle('First message')
-                .setDescription(`If you're seeing this message, that means that the webhook has been successfully setup! `);
-    
-            try {
-                await webhook.send({
-                    embeds: [embed]
-                });
-    
-                const id = webhook.id;
-    
-                if (config.hasOwnProperty('webhookId')) {
-                    config.webhookId.push(id);
-                } else {
-                    config.webhookId = [id];
-                }
+        // Restrict command to admin roles
+        if (interaction.member.permissions.has([
+            PermissionsBitField.Flags.Administrator, 
+            PermissionsBitField.Flags.AddReactions, 
+            PermissionsBitField.Flags.ManageGuild])) {
+
+                const channel = interaction.options.getChannel('channel');
+                const subcommand = interaction.options.getSubcommand();
                 
-                const jsonString = JSON.stringify(config, null, 2);
-                fs.writeFileSync('./config.json', jsonString, err => {
-                    if (err) {
-                        console.log('Error writing file', err);
-                    } else {
-                        console.log('Successfully wrote file');
+                if (subcommand === 'add') {
+                    const webhook = await channel.createWebhook({
+                        name: 'MFC Updates'
+                    });
+            
+                    const embed = new EmbedBuilder()
+                        .setTitle('First message')
+                        .setDescription(`If you're seeing this message, that means that the webhook has been successfully setup! `);
+            
+                    try {
+                        await webhook.send({
+                            embeds: [embed]
+                        });
+            
+                        const id = webhook.id;
+            
+                        if (config.hasOwnProperty('webhookId')) {
+                            config.webhookId.push(id);
+                        } else {
+                            config.webhookId = [id];
+                        }
+                        
+                        const jsonString = JSON.stringify(config, null, 2);
+                        fs.writeFileSync('./config.json', jsonString, err => {
+                            if (err) {
+                                console.log('Error writing file', err);
+                            } else {
+                                console.log('Successfully wrote file');
+                            }
+                        });
+                        
+                        await interaction.reply(`Webhook created at ${channel}!`);
+            
+                    } catch (e) {
+                        await interaction.reply(`Something went wrong. Please try again.`);
+                        console.log(e);
                     }
-                });
-                
-                await interaction.reply(`Webhook created at ${channel}!`);
-    
-            } catch (e) {
-                await interaction.reply(`Something went wrong. Please try again.`);
-                console.log(e);
-            }
 
+                } else if (subcommand === 'remove') {
+                    const webhooks = await channel.fetchWebhooks();
 
-        } else if (subcommand === 'remove') {
-            const webhooks = await channel.fetchWebhooks();
+                    webhooks.forEach(webhook => {
+                        webhook.delete();
 
-            webhooks.forEach(webhook => {
-                webhook.delete();
+                        if (config.hasOwnProperty('webhookId')) {
+                            if (config.webhookId.indexOf(webhook.id) !== -1) {
+                                config.webhookId = config.webhookId.filter(id => id !== webhook.id);
+                            }
+                        }
+                    });
 
-                if (config.hasOwnProperty('webhookId')) {
-                    if (config.webhookId.indexOf(webhook.id) !== -1) {
-                        config.webhookId = config.webhookId.filter(id => id !== webhook.id);
-                    }
+                    const jsonString = JSON.stringify(config, null, 2);
+
+                    fs.writeFileSync('./config.json', jsonString, err => {
+                        if (err) {
+                            console.log('Error writing file', err);
+                        } else {
+                            console.log('Successfully wrote file');
+                        }
+                    });
+
+                    await interaction.reply(`All webhooks removed from ${channel}.`);
                 }
+
+        } else {
+            await interaction.reply({
+                content: 'Sorry, but you do not have permission to use this command.',
+                ephemeral: true
             });
-
-            config.channelId = config.channelId.filter(id => id !== channel);
-
-            const jsonString = JSON.stringify(config, null, 2);
-
-            fs.writeFileSync('./config.json', jsonString, err => {
-                if (err) {
-                    console.log('Error writing file', err);
-                } else {
-                    console.log('Successfully wrote file');
-                }
-            });
-
-            await interaction.reply(`All webhooks removed from ${channel}.`);
         }
     }
 }
